d1f3a87c99c185bb7ecedebc0eee338761a8745b
hoanghungnguyen <hoanghunghm2004@gmail.com>
2025-12-27 11:41:39 +0700

    fix components

---
+++ src/pages/AnnouncementsPage.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { getAnnouncements, markAsRead } from "../api/announcementApi";
+import AnnouncementItem from "../components/AnnouncementItem";
+
+export default function AnnouncementsPage() {
+  const [announcements, setAnnouncements] = useState([]);
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = async () => {
+    const res = await getAnnouncements();
+    setAnnouncements(res.data);
+  };
+
+  const handleRead = async (id) => {
+    await markAsRead(id);
+    loadData();
+  };
+
+  return (
+    <div className="card">
+      <h2>📢 Thông báo từ bệnh viện</h2>
+
+      {announcements.length === 0 && <p>Không có thông báo.</p>}
+
+      {announcements.map((item) => (
+        <AnnouncementItem
+          key={item.id}
+          data={item}
+          onRead={() => handleRead(item.id)}
+        />
+      ))}
+    </div>
+  );
+}
